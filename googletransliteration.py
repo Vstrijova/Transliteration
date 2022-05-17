e7c7942478237362168700c5ecaa5c857d095d1e
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-05-17 16:24:47 +0200

    Update googletransliteration.py

--- googletransliteration.py
+++ googletransliteration.py
@@ -1,7 +1,5 @@
 from google.transliteration import transliterate_text
 import streamlit as st
-google.load("language", "1");
-result = google.language.transliterate(["Namaste"], "en", "hi")
+
+result = transliterate_text('Giochiamo a nascondino',  "ru")
 st.write(result)
-#result = transliterate_text('Giochiamo a nascondino',  "ru")
-#st.write(result)
