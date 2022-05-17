502b50bdc139e585323817e78866706c1cd3df58
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-05-17 13:10:54 +0200

    Update googletransliteration.py

--- googletransliteration.py
+++ googletransliteration.py
@@ -1,13 +1,12 @@
 type="text/javascript"
-  google.load("elements", "1", {
-  packages: "transliterate"
-  })
-  function onLoad() {
-        var options = {
-          sourceLanguage: 'it',
-          destinationLanguage: ['ru'],
-          transliterationEnabled: true
-        };
+google.load("elements", "1", {packages: "transliterate"})
+function onLoad() {
+  var options = {
+    sourceLanguage: 'en',
+    destinationLanguage: ['hi'],
+    shortcutKey: 'ctrl+g',
+    transliterationEnabled: true
+  };
     var control =
             new google.elements.transliteration.TransliterationControl(options);
     var ids = [ "altalena", "nascondino" ];
