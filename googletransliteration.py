7730aaf77c25d478cedbe8b5f5e670b74c87d1ba
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-05-17 13:14:30 +0200

    Update googletransliteration.py

--- googletransliteration.py
+++ googletransliteration.py
@@ -1,13 +1,2 @@
-type="text/javascript"
 google.load("elements", "1", {packages: "transliterate"})
-function onLoad() {
-  var options = {
-    sourceLanguage: 'en',
-    destinationLanguage: ['hi'],
-    shortcutKey: 'ctrl+g',
-    transliterationEnabled: true
-  };
-    var control =
-            new google.elements.transliteration.TransliterationControl(options);
-    var ids = [ "altalena", "nascondino" ];
-        control.makeTransliteratable(ids);
+google.language.transliterate(["altalena"], "it", "ru")
