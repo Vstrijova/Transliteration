18a1385f636a404038d285a110bc143d31774a24
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-05-17 13:03:09 +0200

    Create googletransliteration.py

---
+++ googletransliteration.py
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+  google.load("elements", "1", {
+  packages: "transliterate"
+  });
+</script>
+  function onLoad() {
+        var options = {
+          sourceLanguage: 'it',
+          destinationLanguage: ['ru'],
+          transliterationEnabled: true
+        };
+    var control =
+            new google.elements.transliteration.TransliterationControl(options);
+    var ids = [ "altalena", "nascondino" ];
+        control.makeTransliteratable(ids);
